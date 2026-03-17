dced283294abe8d6f2af56e09b3cf10a0635d7ea
Borisb127 <boris.borochov92@gmail.com>
2026-03-17 17:11:43 +0200

    Add filled icons on active folder

--- apps/mail/cmps/MailSidebar.jsx
+++ apps/mail/cmps/MailSidebar.jsx
@@ -14,29 +14,28 @@ export function MailSidebar({ onSetCompose, unreadCount, draftCount, filterBy, o
             <div className={`folder ${filterBy.status === 'inbox' ? 'active' : ''}`}
                 title="Inbox"
                 onClick={() => onSelectFolder('inbox')}>
-                <img src="assets/img/inbox.svg" alt="" />
-                <span>Inbox</span>
+                <img src={`assets/img/${filterBy.status === 'inbox' ? 'inbox_filled' : 'inbox'}.svg`} alt="" />                <span>Inbox</span>
                 <span className="folder-count">{unreadCount}</span>
             </div>
 
             <div className={`folder ${filterBy.status === 'starred' ? 'active' : ''}`}
                 title="Starred"
                 onClick={() => onSelectFolder('starred')}>
-                <img src="assets/img/star.svg" alt="" />
+                <img src={`assets/img/${filterBy.status === 'starred' ? 'star_filled' : 'star'}.svg`} alt="" />
                 <span>Starred</span>
             </div>
 
             <div className={`folder ${filterBy.status === 'sent' ? 'active' : ''}`}
                 title="Sent"
                 onClick={() => onSelectFolder('sent')}>
-                <img src="assets/img/send.svg" alt="" />
+                <img src={`assets/img/${filterBy.status === 'sent' ? 'send_filled' : 'send'}.svg`} alt="" />
                 <span>Sent</span>
             </div>
 
             <div className={`folder ${filterBy.status === 'draft' ? 'active' : ''}`}
                 title="Draft"
                 onClick={() => onSelectFolder('draft')}>
-                <img src="assets/img/draft.svg" alt="" />
+                <img src={`assets/img/${filterBy.status === 'draft' ? 'draft_filled' : 'draft'}.svg`} alt="" />
                 <span>Drafts</span>
                 <span className="folder-count">{draftCount}</span>
             </div>
@@ -44,7 +43,7 @@ export function MailSidebar({ onSetCompose, unreadCount, draftCount, filterBy, o
             <div className={`folder ${filterBy.status === 'trash' ? 'active' : ''}`}
                 title="Trash"
                 onClick={() => onSelectFolder('trash')}>
-                <img src="assets/img/delete.svg" alt="" />
+                <img src={`assets/img/${filterBy.status === 'trash' ? 'delete_filled' : 'delete'}.svg`} alt="" />
                 <span>Trash</span>
             </div>
         </nav>
